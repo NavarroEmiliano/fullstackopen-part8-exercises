47bb5f4a6c6d11c3cbe001442561a75742ba6cda
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-06-08 11:45:04 -0300

    exercise 8.9.

--- library-frontend-graphql/src/components/Books.jsx
+++ library-frontend-graphql/src/components/Books.jsx
@@ -1,20 +1,31 @@
+import { useQuery } from '@apollo/client'
+import { ALL_BOOKS } from '../queries'
+
 const Books = (props) => {
+  const { loading, error, data } = useQuery(ALL_BOOKS)
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+
+  const books = data?.allBooks && [...data.allBooks]
 
   return (
     <div>
-      <h2>books</h2>
+      <h2>Books</h2>
 
       <table>
         <tbody>
           <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
+            <th> </th>
+            <th>Author</th>
+            <th>Published</th>
           </tr>
           {books.map((a) => (
             <tr key={a.title}>
